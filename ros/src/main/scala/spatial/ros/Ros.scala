a692137f24029cf72cf5689e4572780b48890680
Ruben Fiszel <ruben.fiszel@epfl.ch>
2017-04-22 14:05:55 -0700

    ScalaGen

--- ros/src/main/scala/spatial/ros/Ros.scala
+++ ros/src/main/scala/spatial/ros/Ros.scala
@@ -1,8 +1,10 @@
 package spatial.ros
 
 import argon.codegen.cppgen._
+import argon.codegen.scalagen._
 import spatial.codegen.cppgen._
 import spatial._
+import spatial.codegen.scalagen._
 import spatial.ros.api._
 import spatial.ros.codegen.rosgen._
 import spatial.ros.codegen.scalagen.RosScalaCodegen
@@ -17,8 +19,26 @@ trait RosGen extends RosCodegen with RosFileGen
   with CppGenDRAM with CppGenHostTransfer with CppGenUnrolled with CppGenVector
   with CppGenArray with CppGenArrayExt with CppGenRange with CppGenAlteraVideo with CppGenStream
   with CppGenHashMap with CppGenStructs with CppGenDebugging with CppGenFileIO{
-    override val IR: SpatialCompiler
+    override val IR: RosCompiler
 
 }
 
-trait RosGenScala extends RosScalaCodegen
+trait RosGenScala extends RosScalaCodegen with ScalaFileGen
+  with ScalaGenArray with ScalaGenSpatialArrayExt with ScalaGenSpatialBool with ScalaGenSpatialFixPt with ScalaGenSpatialFltPt
+  with ScalaGenHashMap with ScalaGenIfThenElse with ScalaGenStructs with ScalaGenSpatialStruct
+  with ScalaGenText with ScalaGenVoid with ScalaGenFunction with ScalaGenVariables
+  with ScalaGenDebugging
+  with ScalaGenController with ScalaGenCounter with ScalaGenDRAM with ScalaGenFIFO with ScalaGenHostTransfer with ScalaGenMath
+  with ScalaGenRange with ScalaGenReg with ScalaGenSRAM with ScalaGenUnrolled with ScalaGenVector
+  with ScalaGenStream
+  with ScalaGenLineBuffer with ScalaGenRegFile with ScalaGenStateMachine with ScalaGenFileIO with ScalaGenShiftReg {
+
+    override val IR: RosCompiler
+
+    override def copyDependencies(out: String): Unit = {
+        dependencies ::= FileDep("scalagen", "Makefile", "../")
+        dependencies ::= FileDep("scalagen", "run.sh", "../")
+        dependencies ::= FileDep("scalagen", "build.sbt", "../")
+        super.copyDependencies(out)
+    }
+}
--- ros/src/main/scala/spatial/ros/RosCompiler.scala
+++ ros/src/main/scala/spatial/ros/RosCompiler.scala
@@ -3,7 +3,7 @@ package spatial.ros
 import spatial._
 
 trait RosCompiler extends SpatialCompiler { self =>
-  lazy val rosGenScala = new RosGenScala { val IR: self.type = self; override def shouldRun = RosConfig.enableRosSim}
+  lazy val rosGenScala = new RosGenScala { val IR: self.type = self; override def shouldRun = RosConfig.enableRosSim; def localMems = uctrlAnalyzer.localMems}
   lazy val rosGen = new RosGen { val IR: self.type = self; override def shouldRun = RosConfig.enableRosSynth}
 
   override def createTraversalSchedule() = {
